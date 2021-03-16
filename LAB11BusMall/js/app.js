db0f3e7cb737cb29ee60541ab91462e39ffbde2e
Saadoun Al-dhirat <saadoundhirat93@gmail.com>
2021-03-16 19:32:19 +0200

    solving lab 11 BusMALL

--- LAB11BusMall/js/app.js
+++ LAB11BusMall/js/app.js
@@ -27,12 +27,9 @@ const names = [ bag ,banana,bathroom,boots,breakfast,bubblegum,chair,cthulhu,dog
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-// const names = [ 'bag' ,'banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
-// make golbal variable the holde how many time clecked:
-// let numofclicked = 0 ;// viewed
 
 // GET SECTION ID AND IMAGES ID :
-let sectionid = document.getElementById('imagesSection');
+// let sectionid = document.getElementById('imagesSection');// this will hold images
 let leftimgid = document.getElementById('leftImage');
 let centerimgid = document.getElementById('centerImage');
 let rightimgid = document.getElementById('rightImage');
@@ -61,14 +58,26 @@ for (let i =0 ; i<names.length ;i++){
 function render(){
   let leftpicrandomnum = randomNumber(0 , Busmall.all.length-1); // get random index from busmall.all[]
   let leftrandomimage = Busmall.all[leftpicrandomnum]; // here is object include all properties
+  //
   let centerpicrandomnum = randomNumber(0 , Busmall.all.length-1);
   let centerrandomimage = Busmall.all[centerpicrandomnum];
+  //
   let rightpicrandomnum = randomNumber(0 , Busmall.all.length-1);
   let rightrandomimage = Busmall.all[rightpicrandomnum];
+  // to prevent doublicate
+  if (leftpicrandomnum === centerpicrandomnum || leftpicrandomnum === rightpicrandomnum){
+    centerpicrandomnum = randomNumber(0 , Busmall.all.length-1);
+    centerrandomimage = Busmall.all[centerpicrandomnum];
+  } else if (centerpicrandomnum === rightpicrandomnum) {
+    rightpicrandomnum = randomNumber(0 , Busmall.all.length-1);
+    rightrandomimage = Busmall.all[rightpicrandomnum];
+  }
   // send image src to the image id :
+
   leftimgid.src = leftrandomimage.path; // assign the path to the image source
   leftimgid.titel=leftrandomimage.titel;
   leftimgid.alt=leftrandomimage.titel;
+  console.log();
 
   centerimgid.src = centerrandomimage.path;
   centerimgid.titel=centerrandomimage.titel;
@@ -77,70 +86,61 @@ function render(){
   rightimgid.src = rightrandomimage.path;
   rightimgid.titel=rightrandomimage.titel;
   rightimgid.alt=rightrandomimage.titel;
-}
 
+}
+let counter =0;
 ///////////// add event listener for each image :
-sectionid.addEventListener('click', clickeventlistener);
+leftimgid.addEventListener('click', clickeventlistener);
+centerimgid.addEventListener('click', clickeventlistener);
+rightimgid.addEventListener('click', clickeventlistener);
+
 function clickeventlistener (event){
-  event.preventDefault();
-  if (event.target.id === 'leftimgid' || event.target.id === 'centerimgid' || event.target.id === 'rightimgid'){
+  // event.preventDefault();
+  counter++;
+  if (event.target.id === 'leftImage' || event.target.id === 'centerImage' || event.target.id === 'rightImage'){
     for (let i=0 ; i<Busmall.all.length ;i++){
       if (Busmall.all[i].name === event.target.titel){
         Busmall.all[i].view +=1;
         Busmall.all[i].votes+=1;
-        console.log(Busmall.all[i]);
+        console.log('hhhhhhhhhhh');
+      }else if (Busmall.all[i].name === leftimgid.titel || Busmall.all[i].name === centerimgid.titel || Busmall. all[i].name === rightimgid.titel){
+        Busmall.all[i].view +=1;
       }
     }
-  }render();
+    if (counter === 24 ){
+      clickonbutton.style.display='block';
+      clickonbutton.addEventListener('click',showimagesvotes);
+      leftimgid.removeEventListener('click', clickeventlistener);
+      centerimgid.removeEventListener('click', clickeventlistener);
+      rightimgid.removeEventListener('click', clickeventlistener)
+      console.table(Busmall.all);
+    }render();
+  }
 }
+render();
 
-// function to display the result
-function showimages (){
+//// function to display the result
+function showimagesvotes (){
   let unorderlist = document.createElement('ul');
   for (let i=0 ; i<Busmall.all.length ; i++){
     let listitem = document.createElement('li');
-    listitem.textContent = 'Item name :'+ Busmall.all[i].name +'go number of :' + 'Votes :'+ Busmall.all[i].votes ;
+    listitem.textContent = 'Item name :'+ Busmall.all[i].name +'go number of :' + 'Votes :'+ Busmall.all[i].votes + 'view is ' +Busmall.all[i].view;
+    listitem.style.fontSize='35px';
+    listitem.style.fontFamily='arial';
     unorderlist.appendChild(listitem);
   }
   showtheresult.appendChild(unorderlist);
 }
-
-
-
-
-render();
-
-showimages();
-
-
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-// const numberofclickedimages=[];
-// /////////////////////////////////////make the event listener :
-// sectionid.addEventListener('click', clickhandler);
-// function clickhandler(event){
-//   numofclicked=numofclicked+1; // to calculate number of clickes :
-//   //event.target.id this one will return the id of the target which is one of the images
-//   if (event.target.id === leftimgid || event.target.id === centerimgid || event.target.id === rightimgid){
-//     for (let i=0 ; i<Busmall.all.length ;i++){
-//       if (Busmall.all[i].name === event.target.titel){
-//         Busmall.all[i].clickedtiemimg++;
-//         Busmall.all[i].numviewstime++;
-//         // numberofclickedimages.push[Busmall.all[i].name];
-//       }
-//     }
-//     render();
-//   }
-//   if (clickhandler === 24){
-//     showimages();
-//   }
-// }
-
+/////////////////////////////// making the button and the style//////////////////////////////////////
+let clickonbutton = document.createElement('button');
+clickonbutton.textContent=('button');
+showtheresult.appendChild(clickonbutton);
+clickonbutton.style.display='none';
+clickonbutton.style.alignItems='center';
+clickonbutton.style.width='120px';
+clickonbutton.style.background='rgba(211, 115, 211, 0.8);';
+clickonbutton.style.border='5px dottet red';
+clickonbutton.style.color='red';
+clickonbutton.style.marginLeft='47%';
+clickonbutton.style.padding='40px';
+clickonbutton.style.fontSize='15px';
